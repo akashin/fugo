6bb0ebdd6c54dbba0e95f15f8622eabda7a5ec84
acid <acid@yandex-team.ru>
2016-03-23 11:59:37 +0300

    Pass library path as an argument to combine_headers

--- combine_headers.py
+++ combine_headers.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python2.7
-import sys
 import os
+
+from argparse import ArgumentParser
 
 used = set();
 
-def go(oldFile, path):
+def go(oldFile, library_path, path):
     relative = os.path.join(os.path.dirname(oldFile), path)
+    relative_to_library = os.path.join(library_path, path)
     if os.path.exists(relative):
         return relative
+    elif os.path.exists(relative_to_library):
+        return relative_to_library
     else:
         return path
 
-def dfs(file):
+def dfs(file, library_path):
     if file in used:
         return
     used.add(file)
     if os.path.isdir(file):
         for inner in os.listdir(file):
-            dfs(file + '/' + inner)
+            dfs(file + '/' + inner, library_path)
     else:
         with open(file, "r") as f:
             for line in f:
@@ -31,9 +35,18 @@ def dfs(file):
                     used.add(name)
                     print line,
                 elif line.startswith("#include \""):
-                    dfs(go(file, line[8:].strip("\n\r \"")))
+                    dfs(go(file, library_path, line[8:].strip("\n\r \"")), library_path)
                 else:
                     print line,
 
-for file in sys.argv[1:]:
-    dfs(file)
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("source", type=str, help="Path to source file")
+    parser.add_argument("library", type=str, help="Path to included library")
+    args = parser.parse_args()
+
+    dfs(args.source, args.library)
+
+if __name__ == "__main__":
+    main()
