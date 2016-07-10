7d7121c75eb7e7173318ab936af99517376fd7b8
acid <acid@yandex-team.ru>
2016-07-10 15:59:35 +0300

    Add factorization algorithm. Make gcd template based

--- numeric/primes.hpp
+++ numeric/primes.hpp
@@ -17,7 +17,27 @@ std::vector<int> findDivisors(int x)
     return divisors;
 }
 
-int gcd(int a, int b)
+std::vector<std::pair<int, int>> factorize(int x)
+{
+    std::vector<std::pair<int, int>> factors;
+    for (int i = 2; i * i <= x; ++i) {
+        int degree = 0;
+        while (x % i == 0) {
+            x /= i;
+            ++degree;
+        }
+        if (degree > 0) {
+            factors.push_back(std::make_pair(i, degree));
+        }
+    }
+    if (x != 1) {
+        factors.push_back(std::make_pair(x, 1));
+    }
+    return factors;
+}
+
+template <typename T>
+T gcd(T a, T b)
 {
     if (!a) {
         return b;
